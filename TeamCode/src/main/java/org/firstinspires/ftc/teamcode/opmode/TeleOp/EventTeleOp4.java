d2b5d5b8201ee3201cb2c0a80125352e5491e622
ArushYadlapati <arush.yadlapati@gmail.com>
2025-11-11 17:12:06 -0800

    Added EventTeleOp4

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmode/TeleOp/EventTeleOp4.java
@@ -0,0 +1,252 @@
+package org.firstinspires.ftc.teamcode.opmode.TeleOp;
+
+import static org.firstinspires.ftc.teamcode.commandbase.Deposit.DepositPivotState;
+import static org.firstinspires.ftc.teamcode.commandbase.Deposit.depositPivotState;
+import static org.firstinspires.ftc.teamcode.commandbase.Intake.IntakeMotorState;
+import static org.firstinspires.ftc.teamcode.commandbase.Intake.IntakePivotState;
+import static org.firstinspires.ftc.teamcode.commandbase.Intake.SampleColorDetected;
+import static org.firstinspires.ftc.teamcode.commandbase.Intake.SampleColorTarget;
+import static org.firstinspires.ftc.teamcode.commandbase.Intake.intakePivotState;
+import static org.firstinspires.ftc.teamcode.commandbase.Intake.sampleColor;
+import static org.firstinspires.ftc.teamcode.hardware.Globals.ENDGAME_ASCENT_HEIGHT;
+import static org.firstinspires.ftc.teamcode.hardware.Globals.HIGH_BUCKET_HEIGHT;
+import static org.firstinspires.ftc.teamcode.hardware.Globals.INTAKE_HOLD_SPEED;
+import static org.firstinspires.ftc.teamcode.hardware.Globals.MAX_EXTENDO_EXTENSION;
+import static org.firstinspires.ftc.teamcode.hardware.Globals.OpModeType;
+import static org.firstinspires.ftc.teamcode.hardware.Globals.autoEndPose;
+import static org.firstinspires.ftc.teamcode.hardware.Globals.depositInit;
+import static org.firstinspires.ftc.teamcode.hardware.Globals.opModeType;
+
+import com.pedropathing.localization.Pose;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.Gamepad;
+import com.qualcomm.robotcore.util.ElapsedTime;
+import com.seattlesolvers.solverslib.command.CommandOpMode;
+import com.seattlesolvers.solverslib.command.ConditionalCommand;
+import com.seattlesolvers.solverslib.command.InstantCommand;
+import com.seattlesolvers.solverslib.command.SequentialCommandGroup;
+import com.seattlesolvers.solverslib.command.UninterruptibleCommand;
+import com.seattlesolvers.solverslib.command.WaitCommand;
+import com.seattlesolvers.solverslib.drivebase.MecanumDrive;
+import com.seattlesolvers.solverslib.gamepad.GamepadEx;
+import com.seattlesolvers.solverslib.gamepad.GamepadKeys;
+
+import org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit;
+import org.firstinspires.ftc.teamcode.commandbase.Drive;
+import org.firstinspires.ftc.teamcode.commandbase.Intake;
+import org.firstinspires.ftc.teamcode.commandbase.commands.RealTransfer;
+import org.firstinspires.ftc.teamcode.commandbase.commands.SetDeposit;
+import org.firstinspires.ftc.teamcode.commandbase.commands.SetIntake;
+import org.firstinspires.ftc.teamcode.hardware.Robot;
+import org.firstinspires.ftc.teamcode.hardware.TelemetryData;
+
+import gay.zharel.fateweaver.flight.FlightLogChannel;
+import gay.zharel.fateweaver.flight.FlightRecorder;
+import gay.zharel.fateweaver.schemas.LongSchema;
+
+@TeleOp(name = "EventTeleOp4")
+public class EventTeleOp4 extends CommandOpMode {
+    public GamepadEx driver;
+    public GamepadEx operator;
+
+    public ElapsedTime timer;
+    public ElapsedTime gameTimer;
+
+    TelemetryData telemetryData = new TelemetryData(telemetry);
+
+    private final Robot robot = Robot.getInstance();
+
+    private boolean endgame = false;
+
+    FlightLogChannel<Long> timestamps;
+
+    @Override
+    public void initialize() {
+        // Must have for all opModes
+        opModeType = OpModeType.TELEOP;
+        depositInit = DepositPivotState.MIDDLE_HOLD;
+
+        INTAKE_HOLD_SPEED = 0;
+
+        // DO NOT REMOVE! Resetting FTCLib Command Scheduler
+        super.reset();
+
+        robot.init(hardwareMap);
+
+        // Initialize subsystems
+        register(robot.deposit, robot.intake);
+
+        robot.intake.setActiveIntake(IntakeMotorState.STOP);
+
+        driver = new GamepadEx(gamepad1);
+        operator = new GamepadEx(gamepad2);
+
+        // Driver Gamepad controls
+        driver.getGamepadButton(GamepadKeys.Button.CIRCLE).whenPressed(
+                new InstantCommand(() -> robot.intake.toggleActiveIntake(SampleColorTarget.ANY_COLOR))
+        );
+
+        driver.getGamepadButton(GamepadKeys.Button.CROSS).whenPressed(
+                new InstantCommand(() -> robot.intake.toggleActiveIntake(SampleColorTarget.ALLIANCE_ONLY))
+        );
+
+        driver.getGamepadButton(GamepadKeys.Button.SQUARE).whenPressed(
+                new SetIntake(robot, IntakePivotState.INTAKE, IntakeMotorState.FORWARD, MAX_EXTENDO_EXTENSION / 2, true)
+        );
+
+        driver.getGamepadButton(GamepadKeys.Button.TRIANGLE).whenPressed(
+                new SetIntake(robot, IntakePivotState.INTAKE, IntakeMotorState.FORWARD, MAX_EXTENDO_EXTENSION, true)
+        );
+
+        driver.getGamepadButton(GamepadKeys.Button.DPAD_UP).whenPressed(
+                new InstantCommand(() -> robot.follower.setPose(new Pose(0, 0, 0)))
+        );
+
+        driver.getGamepadButton(GamepadKeys.Button.DPAD_DOWN).whenPressed(
+                new SequentialCommandGroup(
+                        new InstantCommand(() -> robot.intake.setPivot(IntakePivotState.INTAKE_READY)),
+                        new InstantCommand(() -> robot.intake.setActiveIntake(IntakeMotorState.HOLD))
+                )
+        );
+
+        driver.getGamepadButton(GamepadKeys.Button.DPAD_RIGHT).whenPressed(
+                new InstantCommand(() -> robot.intake.setExtendoTarget(0))
+        );
+
+        driver.getGamepadButton(GamepadKeys.Button.LEFT_BUMPER).whenPressed(
+                new ConditionalCommand(
+                        new SetDeposit(robot, DepositPivotState.SCORING, HIGH_BUCKET_HEIGHT, false).withTimeout(1500),
+                        new SequentialCommandGroup(
+                                new InstantCommand(() -> robot.deposit.setClawOpen(true)),
+                                new WaitCommand(300),
+                                new SetDeposit(robot, DepositPivotState.MIDDLE_HOLD, 0, true).withTimeout(1500)
+                        ),
+                        () -> robot.deposit.target == 0
+                )
+        );
+
+        driver.getGamepadButton(GamepadKeys.Button.RIGHT_BUMPER).whenPressed(
+                new InstantCommand(() -> robot.intake.setPivot(IntakePivotState.INTAKE))
+        );
+
+        driver.getGamepadButton(GamepadKeys.Button.RIGHT_STICK_BUTTON).whenPressed(
+                new UninterruptibleCommand(
+                        new RealTransfer(robot)
+                )
+        );
+
+        driver.getGamepadButton(GamepadKeys.Button.PS).whenPressed(
+                new ConditionalCommand(
+                        new SequentialCommandGroup(
+                                new InstantCommand(() -> robot.drive.setHang(Drive.HangState.EXTEND)),
+                                new WaitCommand(3000),
+                                new InstantCommand(() -> robot.drive.setHang(Drive.HangState.STOP)),
+                                new InstantCommand(() -> endgame = true)
+                        ),
+                        new SequentialCommandGroup(
+                                new InstantCommand(() -> robot.drive.setHang(Drive.HangState.RETRACT)),
+                                new SetDeposit(robot, DepositPivotState.INSIDE, ENDGAME_ASCENT_HEIGHT, false)
+                        ),
+                        (() -> !endgame)
+                )
+        );
+
+        super.run();
+    }
+
+    @Override
+    public void run() {
+        // Keep all the has movement init for until when TeleOp starts
+        // This is like the init but when the program is actually started
+        if (timer == null) {
+            robot.initHasMovement();
+
+            INTAKE_HOLD_SPEED = 0.15; // Enable hold
+
+            timer = new ElapsedTime();
+            gameTimer = new ElapsedTime();
+        }
+
+        if (sampleColor.equals(SampleColorDetected.RED)) {
+            gamepad1.setLedColor(1, 0, 0, Gamepad.LED_DURATION_CONTINUOUS);
+        } else if (sampleColor.equals(SampleColorDetected.BLUE)) {
+            gamepad1.setLedColor(0, 0, 1, Gamepad.LED_DURATION_CONTINUOUS);
+        } else if (sampleColor.equals(SampleColorDetected.YELLOW)) {
+            gamepad1.setLedColor(1, 1, 0, Gamepad.LED_DURATION_CONTINUOUS);
+        } else {
+            gamepad1.setLedColor(0, 0, 0, Gamepad.LED_DURATION_CONTINUOUS);
+        }
+
+        // Control Hub IMU Field Centric Code
+
+        double speedMultiplier = 0.35 + (0.65 * driver.getTrigger(GamepadKeys.Trigger.LEFT_TRIGGER));
+
+        double y = driver.getLeftY();
+        double x = driver.getLeftX();
+        double rx = driver.getRightX();
+
+        double botHeading = robot.getYawDegrees();
+
+        double rotX = x * Math.cos(-botHeading) - y * Math.sin(-botHeading);
+        double rotY = x * Math.sin(-botHeading) + y * Math.cos(-botHeading);
+
+        rotX = rotX * 1.1;
+
+        double denominator = Math.max(Math.abs(rotY) + Math.abs(rotX) + Math.abs(rx), 1);
+        double frontLeftPower = (rotY + rotX + rx) / denominator;
+        double backLeftPower = (rotY - rotX + rx) / denominator;
+        double frontRightPower = (rotY - rotX - rx) / denominator;
+        double backRightPower = (rotY + rotX - rx) / denominator;
+
+        robot.leftFront.setPower(frontLeftPower * speedMultiplier);
+        robot.leftBack.setPower(backLeftPower * speedMultiplier);
+        robot.rightFront.setPower(frontRightPower * speedMultiplier);
+        robot.rightBack.setPower(backRightPower * speedMultiplier);
+
+        // DO NOT REMOVE! Runs FTCLib Command Scheduler
+        super.run();
+
+        FlightRecorder.INSTANCE.addData("timer", "init");
+        FlightRecorder.INSTANCE.update();
+        FlightRecorder.INSTANCE.addData("autoEndPose", autoEndPose.toString());
+        FlightRecorder.INSTANCE.addData("extendoReached", robot.intake.extendoReached);
+        FlightRecorder.INSTANCE.addData("extendoRetracted", robot.intake.extendoRetracted);
+        FlightRecorder.INSTANCE.addData("slidesRetracted", robot.deposit.slidesRetracted);
+        FlightRecorder.INSTANCE.addData("slidesReached", robot.deposit.slidesReached);
+        FlightRecorder.INSTANCE.addData("opModeType", opModeType.name());
+
+        FlightRecorder.INSTANCE.addData("hasSample()", robot.intake.hasSample());
+        FlightRecorder.INSTANCE.addData("colorSensor getDistance", robot.colorSensor.getDistance(DistanceUnit.CM));
+        FlightRecorder.INSTANCE.addData("Intake sampleColor", Intake.sampleColor);
+        FlightRecorder.INSTANCE.addData("correctSampleDetected", Intake.correctSampleDetected());
+        FlightRecorder.INSTANCE.addData("intakeMotorState", Intake.intakeMotorState);
+
+        FlightRecorder.INSTANCE.addData("liftTop.getPower()", robot.liftTop.getPower());
+        FlightRecorder.INSTANCE.addData("liftBottom.getPower()", robot.liftBottom.getPower());
+        FlightRecorder.INSTANCE.addData("extension.getPower()", robot.extension.getPower());
+
+        FlightRecorder.INSTANCE.addData("getExtendoScaledPosition()", robot.intake.getExtendoScaledPosition());
+        FlightRecorder.INSTANCE.addData("getLiftScaledPosition()", robot.deposit.getLiftScaledPosition());
+
+        FlightRecorder.INSTANCE.addData("slides target", robot.deposit.target);
+        FlightRecorder.INSTANCE.addData("extendo target", robot.intake.target);
+
+        FlightRecorder.INSTANCE.addData("intakePivotState", intakePivotState);
+        FlightRecorder.INSTANCE.addData("depositPivotState", depositPivotState);
+        FlightRecorder.INSTANCE.addData("Sigma", "Oscar");
+        FlightRecorder.INSTANCE.addData("botHeading", botHeading);
+        FlightRecorder.INSTANCE.addData("speedMultiplier", speedMultiplier);
+
+        FlightRecorder.INSTANCE.update(); // DO NOT REMOVE! Needed for telemetry
+        timer.reset();
+        // DO NOT REMOVE! Removing this will return stale data since bulk caching is on Manual mode
+        // Also only clearing the control hub to decrease loop times
+        // This means if we start reading both hubs (which we aren't) we need to clear both
+        robot.ControlHub.clearBulkCache();
+    }
+
+    @Override
+    public void end() {
+        autoEndPose = robot.follower.getPose();
+    }
+}
